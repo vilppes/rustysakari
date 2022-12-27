fa4e00d50bf16d5ae09b9fde30562f6cbaf81c0b
Pessi Vilppolahti <pessiv@outlook.com>
2022-12-27 19:43:39 +0200

    updated rustysakari to use the functions

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,27 @@
 use std::io; 
-fn check_win(winning: String, answer: String) {
+fn check_win(winning: String, answer: String) -> bool {
     if answer == winning {
-        println!("Hihhihhii kutittaa");
+        return true;
     } else {
-        println!("Hävisit pelin");
+        return false;
     }
 }
 fn handle_user_input() -> String {
     let mut answer = String::new();
     match io::stdin().read_line(&mut answer) {
         Ok(_n) => return answer,
-        Err(error) => return String::from("Haistappa paska t. sakari {}"),
+        Err(_error) => return String::from("Haistappa paska t. sakari {}"),
+    }
+}
+fn game() {
+    let winning = String::from("joo\n");
+    println!("Pue sakarille villapaita (Joo/En)");
+    if check_win(winning, handle_user_input()) {
+        println!("Hihhihhii kutittaa");
+    } else {
+        println!("Hävisit pelin");
     }
 }
 fn main() {
-    let winning = String::from("joo\n");
-    println!("Pue sakarille villapaita");
-    let mut answer = String::new();
-    match io::stdin().read_line(&mut answer) {
-        Ok(_n) => {
-            check_win(winning, answer);
-        },
-        Err(error) => println!("Haistappa paska t. sakari {}", error),
-    }
+    game();
 }
