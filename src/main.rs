ac27cb7c2cfdf38412556bce53c3883125e771df
Pessi Vilppolahti <pessiv@outlook.com>
2022-12-27 22:49:48 +0200

    improved the readability

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod extractable_functions;
 fn game() {
     extractable_functions::states_handler(
-        extractable_functions::check_win(String::from("joo\n"), 
-        extractable_functions::handle_user_input(String::from("Pue sakarille villapaita (joo/ei)"))), 
+        extractable_functions::check_win(
+            String::from("joo\n"), 
+            extractable_functions::handle_user_input(String::from("Pue sakarille villapaita (joo/ei)"))
+        ), 
         String::from("Hihhihhii kutittaa"), 
         String::from("Hävisit pelin")
     )
